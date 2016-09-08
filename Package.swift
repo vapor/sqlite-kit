73254ff711a5c761439d4f3d7a9327db3ce28fad
Tanner Nelson <me@tanner.xyz>
2016-09-08 12:38:48 -0400

    09-06 update

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "SQLite",
     dependencies: [ 
         .Package(url: "https://github.com/vapor/csqlite.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 0),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 0, minor: 5),
     ]
 )
