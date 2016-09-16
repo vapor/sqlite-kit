65171d05da451dd5b926c17b8b4d0636fc4cbc04
Adrien Cantérot <adrien.canterot@gmail.com>
2016-09-16 19:01:59 +0200

    csqlite 1.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SQLite",
     dependencies: [
-        .Package(url: "https://github.com/vapor/csqlite.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/vapor/csqlite.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/core.git", majorVersion: 1),
 
     ]
