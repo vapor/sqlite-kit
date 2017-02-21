59fd6eb9b7c053c77e3f47839e97921b1bd2f18a
Logan Wright <logan.william.wright@gmail.com>
2017-02-21 13:01:46 +0100

    update dependencies

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         Target(name: "SQLite", dependencies: ["CSQLite"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/node", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/node", Version(2,0,0, prereleaseIdentifiers: ["alpha"]))
     ]
 )
