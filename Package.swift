7b3ad8ff837e3ac01bc7da82f4ce41f13c7787af
Logan Wright <logan.william.wright@gmail.com>
2017-02-21 15:54:36 +0100

    node.git cuz reasons

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
-        .Package(url: "https://github.com/vapor/node", Version(2,0,0, prereleaseIdentifiers: ["alpha"]))
+        .Package(url: "https://github.com/vapor/node.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"]))
     ]
 )
