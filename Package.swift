3a7608225a4c9328ad426ef64341c37c2796ab75
Logan Wright <logan.william.wright@gmail.com>
2017-05-17 18:12:47 +0100

    package/readme

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         Target(name: "SQLite", dependencies: ["CSQLite"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/node.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
     ]
 )
