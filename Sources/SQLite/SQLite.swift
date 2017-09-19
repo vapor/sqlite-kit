ef50e8b2ce37acb0d51d31a94c22b18a545c7868
Daniel Santos <dsantosp12@gmail.com>
2017-09-18 22:04:10 -0400

    Added a more complete errors identifiers relative to the SQLite3's API (cont'd).

--- Sources/SQLite/SQLite.swift
+++ Sources/SQLite/SQLite.swift
@@ -78,7 +78,7 @@ public class SQLite {
         }
 
         guard let statementPointer = statementContainer.pointee else {
-            throw SQLiteError(with: SQLITE_ERROR, msg: "Statement pointer error")
+            throw SQLiteError(with: SQLITE_ERROR, msg: "Statement pointer error")!
         }
 
         let statement = Statement(pointer: statementPointer, database: database)
