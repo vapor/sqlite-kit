159d4971eb4b7565f49853ca8c786534d21f6cc2
Tanner Nelson <me@tanner.xyz>
2018-02-23 19:15:20 -0500

    async package

--- Package.swift
+++ Package.swift
@@ -7,12 +7,15 @@ let package = Package(
         .library(name: "SQLite", targets: ["SQLite"]),
     ],
     dependencies: [
+        // ⏱ Promises and reactive-streams in Swift built for high-performance and scalability.
+        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
+
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
         .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
     ],
     targets: [
         .target(name: "CSQLite"),
-        .target(name: "SQLite", dependencies: ["Bits", "CodableKit", "CSQLite", "Debugging"]),
+        .target(name: "SQLite", dependencies: ["Async", "Bits", "CodableKit", "CSQLite", "Debugging"]),
         .testTarget(name: "SQLiteTests", dependencies: ["SQLite"]),
     ]
 )
