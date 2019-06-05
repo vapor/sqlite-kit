1920a8e3da0df32b6067397c46855529a48a4764
Tanner Nelson <me@tanner.xyz>
2019-06-05 18:25:32 -0400

    add tests

--- Tests/SQLiteKitTests/SQLKitTests.swift
+++ Tests/SQLiteKitTests/SQLKitTests.swift
@@ -4,13 +4,83 @@ import XCTest
 
 class SQLiteTests: XCTestCase {
     func testBenchmark() throws {
-        try self.benchmarker.run()
+        try self.db.drop(table: "planets")
+            .ifExists()
+            .run().wait()
+        try self.db.drop(table: "galaxies")
+            .ifExists()
+            .run().wait()
+        try self.db.create(table: "galaxies")
+            .column("id", type: .int, .primaryKey)
+            .column("name", type: .text)
+            .run().wait()
+        try self.db.create(table: "planets")
+            .ifNotExists()
+            .column("id", type: .int, .primaryKey)
+            .column("galaxyID", type: .int, .references("galaxies", "id"))
+            .run().wait()
+        try self.db.alter(table: "planets")
+            .column("name", type: .text, .default(SQLLiteral.string("Unamed Planet")))
+            .run().wait()
+        try self.db.create(index: "test_index")
+            .on("planets")
+            .column("id")
+            .unique()
+            .run().wait()
+        // INSERT INTO "galaxies" ("id", "name") VALUES (DEFAULT, $1)
+        try self.db.insert(into: "galaxies")
+            .columns("id", "name")
+            .values(SQLLiteral.null, SQLBind("Milky Way"))
+            .values(SQLLiteral.null, SQLBind("Andromeda"))
+            // .value(Galaxy(name: "Milky Way"))
+            .run().wait()
+        // SELECT * FROM galaxies WHERE name != NULL AND (name == ? OR name == ?)
+        _ = try self.db.select()
+            .column("*")
+            .from("galaxies")
+            .where("name", .notEqual, SQLLiteral.null)
+            .where {
+                $0.where("name", .equal, SQLBind("Milky Way"))
+                    .orWhere("name", .equal, SQLBind("Andromeda"))
+            }
+            .all().wait()
+
+        _ = try self.db.select()
+            .column("*")
+            .from("galaxies")
+            .where(SQLColumn("name"), .equal, SQLBind("Milky Way"))
+            .groupBy("id")
+            .orderBy("name", .descending)
+            .all().wait()
+
+        try self.db.insert(into: "planets")
+            .columns("id", "name")
+            .values(SQLLiteral.null, SQLBind("Earth"))
+            .run().wait()
+
+        try self.db.insert(into: "planets")
+            .columns("id", "name")
+            .values(SQLLiteral.null, SQLBind("Mercury"))
+            .values(SQLLiteral.null, SQLBind("Venus"))
+            .values(SQLLiteral.null, SQLBind("Mars"))
+            .values(SQLLiteral.null, SQLBind("Jpuiter"))
+            .values(SQLLiteral.null, SQLBind("Pluto"))
+            .run().wait()
+
+        try self.db.select()
+            .column(SQLFunction("count", args: "name"))
+            .from("planets")
+            .where("galaxyID", .equal, SQLBind(5))
+            .run().wait()
+
+        try self.db.select()
+            .column(SQLFunction("count", args: SQLLiteral.all))
+            .from("planets")
+            .where("galaxyID", .equal, SQLBind(5))
+            .run().wait()
     }
 
-    var benchmarker: SQLBenchmarker<ConnectionPool<SQLiteConnectionSource>> {
-        return SQLBenchmarker.init(on: self.connectionPool)
-    }
-    var connectionPool: ConnectionPool<SQLiteConnectionSource>!
+    var db: ConnectionPool<SQLiteConnectionSource>!
     var threadPool: NIOThreadPool!
     var eventLoopGroup: EventLoopGroup!
 
@@ -18,12 +88,12 @@ class SQLiteTests: XCTestCase {
         self.eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
         self.threadPool = .init(numberOfThreads: 2)
         let db = SQLiteConnectionSource(configuration: .init(storage: .memory), threadPool: self.threadPool, on: self.eventLoopGroup.next())
-        self.connectionPool = ConnectionPool(config: .init(maxConnections: 8), source: db)
+        self.db = ConnectionPool(config: .init(maxConnections: 8), source: db)
     }
 
     override func tearDown() {
-        try! self.connectionPool.close().wait()
-        self.connectionPool = nil
+        try! self.db.close().wait()
+        self.db = nil
         try! self.threadPool.syncShutdownGracefully()
         self.threadPool = nil
         try! self.eventLoopGroup.syncShutdownGracefully()
