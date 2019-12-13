378f866e0ac032c47890b5b4881e537cd8a69bdf
tanner0101 <me@tanner.xyz>
2019-12-13 16:45:24 -0500

    sql row protocol updates

--- Sources/SQLiteKit/SQLiteRow+SQLRow.swift
+++ Sources/SQLiteKit/SQLiteRow+SQLRow.swift
@@ -1,12 +1,24 @@
 extension SQLiteRow: SQLRow {
+    public var allColumns: [String] {
+        .init(self.columns.offsets.keys)
+    }
+
+    public func decodeNil(column: String) throws -> Bool {
+        self.columns.offsets.keys.contains(column)
+    }
+
     public func contains(column: String) -> Bool {
         return self.column(column) != nil
     }
 
     public func decode<D>(column: String, as type: D.Type) throws -> D where D : Decodable {
         guard let data = self.column(column) else {
-            fatalError("no value found for \(column)")
+            throw MissingColumn(column: column)
         }
         return try SQLiteDataDecoder().decode(D.self, from: data)
     }
+}
+
+struct MissingColumn: Error {
+    let column: String
 }
