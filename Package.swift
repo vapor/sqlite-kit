405dd3feab1f975cc65de9eefdd8cd9d2dce1440
logan <logan.william.wright@gmail.com>
2020-07-29 16:23:41 +0200

    add minimum ios version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "sqlite-kit",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_15),
+       .iOS(.v13),
     ],
     products: [
         .library(name: "SQLiteKit", targets: ["SQLiteKit"]),
