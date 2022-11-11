8e94c3264984e718f9cfe8e30d5617de6430c256
Gwynne Raskind <gwynne@vapor.codes>
2022-11-11 16:53:25 -0600

    Do a mostly visual update to SQLiteDialect, removing quite a lot of confusing whitespace and simplifying most of the content. Adds a `customDataType(for:)` override to handling requests for bigint types and switches from plain `?` for placeholders to SQLite's numbered `?NNNN` representation per SQLite docs recommendation.

--- Sources/SQLiteKit/SQLiteDialect.swift
+++ Sources/SQLiteKit/SQLiteDialect.swift
@@ -1,58 +1,38 @@
+import SQLKit
+
+/// The ``SQLDialect`` defintions for SQLite.
+///
+/// - Note: There is only ever one SQLite library in use by SQLiteNIO in any given process (even if there are
+///   other versions of the library being used by other things). As such, there is no need for the dialect to
+///   concern itself with what version the connection using it "connected" to - it can always just look up the
+///   global "runtime version".
 public struct SQLiteDialect: SQLDialect {
-    public var name: String {
-        "sqlite"
-    }
+    public var name: String { "sqlite" }
     
-    public var identifierQuote: SQLExpression {
-        return SQLRaw("\"")
-    }
+    public var identifierQuote: SQLExpression { SQLRaw("\"") }
+    public var literalStringQuote: SQLExpression { SQLRaw("'") }
+    public func bindPlaceholder(at position: Int) -> SQLExpression { SQLRaw("?\(position)") }
+    public func literalBoolean(_ value: Bool) -> SQLExpression { SQLRaw(value ? "TRUE" : "FALSE") }
+    public var literalDefault: SQLExpression { SQLLiteral.null }
 
-    public var literalStringQuote: SQLExpression {
-        return SQLRaw("'")
-    }
+    public var supportsAutoIncrement: Bool { false }
+    public var autoIncrementClause: SQLExpression { SQLRaw("AUTOINCREMENT") }
 
-    public var autoIncrementClause: SQLExpression {
-        return SQLRaw("AUTOINCREMENT")
-    }
-
-    public func bindPlaceholder(at position: Int) -> SQLExpression {
-        return SQLRaw("?")
-    }
-
-    public func literalBoolean(_ value: Bool) -> SQLExpression {
-        switch value {
-        case true: return SQLRaw("TRUE")
-        case false: return SQLRaw("FALSE")
+    public var enumSyntax: SQLEnumSyntax { .unsupported }
+    public var triggerSyntax: SQLTriggerSyntax { .init(create: [.supportsBody, .supportsCondition]) }
+    public var alterTableSyntax: SQLAlterTableSyntax { .init(allowsBatch: false) }
+    public var unionFeatures: SQLUnionFeatures { [.union, .unionAll, .intersect, .except] }
+    
+    public func customDataType(for dataType: SQLDataType) -> SQLExpression? {
+        if case .bigint = dataType {
+            // Translate requests for bigint to requests for SQLite's plain integer type. This yields the autoincrement
+            // primary key behavior when a 64-bit integer is requested from a higher layer.
+            return SQLDataType.int
         }
-    }
-
-    public var literalDefault: SQLExpression {
-        return SQLLiteral.null
-    }
-
-    public var enumSyntax: SQLEnumSyntax {
-        .unsupported
-    }
-
-    public var supportsAutoIncrement: Bool {
-        false
-    }
-
-    public var alterTableSyntax: SQLAlterTableSyntax {
-        .init(
-            alterColumnDefinitionClause: nil,
-            alterColumnDefinitionTypeKeyword: nil,
-            allowsBatch: false
-        )
-    }
-
-    public var triggerSyntax: SQLTriggerSyntax {
-        return .init(create: [.supportsBody, .supportsCondition])
-    }
-    
-    public var unionFeatures: SQLUnionFeatures {
-        [.union, .unionAll, .intersect, .except]
+        return nil
     }
 
     public init() { }
+    
+    }
 }
