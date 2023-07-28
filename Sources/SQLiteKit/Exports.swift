25b05d74c00f4489a12307be3f1b6f77fad33fd6
Gwynne Raskind <gwynne@vapor.codes>
2023-07-27 23:22:10 -0500

    Add CODEOWNERS, fixup Exports.swift

--- Sources/SQLiteKit/Exports.swift
+++ Sources/SQLiteKit/Exports.swift
@@ -1,4 +1,11 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import SQLKit
+@_documentation(visibility: internal) @_exported import SQLiteNIO
+@_documentation(visibility: internal) @_exported import AsyncKit
+@_documentation(visibility: internal) @_exported import struct Logging.Logger
+
+#else
 
 @_exported import SQLKit
 @_exported import SQLiteNIO
