45baf81634db1004c19609f98b5919bdc016b465
Gwynne Raskind <gwynne@vapor.codes>
2023-09-15 01:12:23 -0500

    Convert (most of) the tests to async, add additional tests to improve coverage (tests that actually exercise the appropriate code, not just for better coverage counts).

--- Tests/SQLiteKitTests/SQLiteKitTests.swift
+++ Tests/SQLiteKitTests/SQLiteKitTests.swift
@@ -11,43 +11,39 @@ final class SQLiteKitTests: XCTestCase {
         try benchmark.run()
     }
     
-    func testEnum() throws {
-        try self.benchmark.testEnum()
-    }
-
-    func testPlanets() throws {
-        try self.db.drop(table: "planets")
+    func testPlanets() async throws {
+        try await self.db.drop(table: "planets")
             .ifExists()
-            .run().wait()
-        try self.db.drop(table: "galaxies")
+            .run()
+        try await self.db.drop(table: "galaxies")
             .ifExists()
-            .run().wait()
-        try self.db.create(table: "galaxies")
+            .run()
+        try await self.db.create(table: "galaxies")
             .column("id", type: .int, .primaryKey)
             .column("name", type: .text)
-            .run().wait()
-        try self.db.create(table: "planets")
+            .run()
+        try await self.db.create(table: "planets")
             .ifNotExists()
             .column("id", type: .int, .primaryKey)
             .column("galaxyID", type: .int, .references("galaxies", "id"))
-            .run().wait()
-        try self.db.alter(table: "planets")
+            .run()
+        try await self.db.alter(table: "planets")
             .column("name", type: .text, .default(SQLLiteral.string("Unamed Planet")))
-            .run().wait()
-        try self.db.create(index: "test_index")
+            .run()
+        try await self.db.create(index: "test_index")
             .on("planets")
             .column("id")
             .unique()
-            .run().wait()
+            .run()
         // INSERT INTO "galaxies" ("id", "name") VALUES (DEFAULT, $1)
-        try self.db.insert(into: "galaxies")
+        try await self.db.insert(into: "galaxies")
             .columns("id", "name")
             .values(SQLLiteral.null, SQLBind("Milky Way"))
             .values(SQLLiteral.null, SQLBind("Andromeda"))
             // .value(Galaxy(name: "Milky Way"))
-            .run().wait()
+            .run()
         // SELECT * FROM galaxies WHERE name != NULL AND (name == ? OR name == ?)
-        _ = try self.db.select()
+        _ = try await self.db.select()
             .column("*")
             .from("galaxies")
             .where("name", .notEqual, SQLLiteral.null)
@@ -55,52 +51,66 @@ final class SQLiteKitTests: XCTestCase {
                 $0.where("name", .equal, SQLBind("Milky Way"))
                     .orWhere("name", .equal, SQLBind("Andromeda"))
             }
-            .all().wait()
+            .all()
 
-        _ = try self.db.select()
+        _ = try await self.db.select()
             .column("*")
             .from("galaxies")
             .where(SQLColumn("name"), .equal, SQLBind("Milky Way"))
             .groupBy("id")
             .orderBy("name", .descending)
-            .all().wait()
+            .all()
 
-        try self.db.insert(into: "planets")
+        try await self.db.insert(into: "planets")
             .columns("id", "name")
             .values(SQLLiteral.null, SQLBind("Earth"))
-            .run().wait()
+            .run()
 
-        try self.db.insert(into: "planets")
+        try await self.db.insert(into: "planets")
             .columns("id", "name")
             .values(SQLLiteral.null, SQLBind("Mercury"))
             .values(SQLLiteral.null, SQLBind("Venus"))
             .values(SQLLiteral.null, SQLBind("Mars"))
             .values(SQLLiteral.null, SQLBind("Jpuiter"))
             .values(SQLLiteral.null, SQLBind("Pluto"))
-            .run().wait()
+            .run()
 
-        try self.db.select()
+        try await self.db.select()
             .column(SQLFunction("count", args: "name"))
             .from("planets")
             .where("galaxyID", .equal, SQLBind(5))
-            .run().wait()
+            .run()
 
-        try self.db.select()
+        try await self.db.select()
             .column(SQLFunction("count", args: SQLLiteral.all))
             .from("planets")
             .where("galaxyID", .equal, SQLBind(5))
-            .run().wait()
+            .run()
     }
 
-    func testForeignKeysEnabled() throws {
-        let res = try self.connection.query("PRAGMA foreign_keys").wait()
+    func testForeignKeysEnabledOnlyWhenRequested() async throws {
+        let res = try await self.connection.query("PRAGMA foreign_keys").get()
         XCTAssertEqual(res[0].column("foreign_keys"), .integer(1))
+
+        // Using `.file` storage here is a quick and dirty nod to increasing test coverage.
+        let source = SQLiteConnectionSource(
+            configuration: .init(storage: .file(
+                path: FileManager.default.temporaryDirectory.appendingPathComponent("\(UUID()).sqlite3", isDirectory: false).path
+            ), enableForeignKeys: false),
+            threadPool: self.threadPool
+        )
+
+        let conn2 = try await source.makeConnection(logger: self.connection.logger, on: self.eventLoopGroup.any()).get()
+        defer { try! conn2.close().wait() }
+        
+        let res2 = try await conn2.query("PRAGMA foreign_keys").get()
+        XCTAssertEqual(res2[0].column("foreign_keys"), .integer(0))
     }
 
-    func testJSONStringColumn() throws {
-        _ = try self.connection.query("CREATE TABLE foo (bar TEXT)").wait()
-        _ = try self.connection.query(#"INSERT INTO foo (bar) VALUES ('{"baz": "qux"}')"#).wait()
-        let rows = try self.connection.query("SELECT * FROM foo").wait()
+    func testJSONStringColumn() async throws {
+        _ = try await self.connection.query("CREATE TABLE foo (bar TEXT)").get()
+        _ = try await self.connection.query(#"INSERT INTO foo (bar) VALUES ('{"baz": "qux"}')"#).get()
+        let rows = try await self.connection.query("SELECT * FROM foo").get()
 
         struct Bar: Codable {
             var baz: String
@@ -109,7 +119,7 @@ final class SQLiteKitTests: XCTestCase {
         XCTAssertEqual(bar.baz, "qux")
     }
 
-    func testMultipleInMemoryDatabases() throws {
+    func testMultipleInMemoryDatabases() async throws {
         let a = SQLiteConnectionSource(
             configuration: .init(storage: .memory, enableForeignKeys: true),
             threadPool: self.threadPool
@@ -119,30 +129,93 @@ final class SQLiteKitTests: XCTestCase {
             threadPool: self.threadPool
         )
 
-        let a1 = try a.makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).wait()
+        let a1 = try await a.makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).get()
         defer { try! a1.close().wait() }
-        let a2 = try a.makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).wait()
+        let a2 = try await a.makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).get()
         defer { try! a2.close().wait() }
-        let b1 = try b.makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).wait()
+        let b1 = try await b.makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).get()
         defer { try! b1.close().wait() }
-        let b2 = try b.makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).wait()
+        let b2 = try await b.makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).get()
         defer { try! b2.close().wait() }
 
-        _ = try a1.query("CREATE TABLE foo (bar INTEGER)").wait()
-        _ = try a2.query("SELECT * FROM foo").wait()
-        _ = try b1.query("CREATE TABLE foo (bar INTEGER)").wait()
-        _ = try b2.query("SELECT * FROM foo").wait()
+        _ = try await a1.query("CREATE TABLE foo (bar INTEGER)").get()
+        _ = try await a2.query("SELECT * FROM foo").get()
+        _ = try await b1.query("CREATE TABLE foo (bar INTEGER)").get()
+        _ = try await b2.query("SELECT * FROM foo").get()
     }
 
     // https://github.com/vapor/sqlite-kit/issues/56
-    func testDoubleConstraintError() throws {
-        try self.db.create(table: "foo")
+    func testDoubleConstraintError() async throws {
+        try await self.db.create(table: "foo")
             .ifNotExists()
             .column("id", type: .text, .primaryKey(autoIncrement: false), .notNull)
             .run()
-            .wait()
     }
-
+    
+    func testRowDecoding() async throws {
+        try await self.db.create(table: "foo")
+            .column("id", type: .int, .primaryKey(autoIncrement: false), .notNull)
+            .column("value", type: .text)
+            .run()
+        try await self.db.insert(into: "foo")
+            .columns("id", "value")
+            .values(SQLLiteral.numeric("1"), SQLBind("abc"))
+            .values(SQLLiteral.numeric("2"), SQLBind(String?.none))
+            .run()
+        let rows = try await self.db.select()
+            .column("value")
+            .from("foo")
+            .orderBy("id")
+            .all()
+        
+        XCTAssertEqual(rows.count, 2)
+        let row1 = try XCTUnwrap(rows.dropFirst(0).first),
+            row2 = try XCTUnwrap(rows.dropFirst(1).first)
+        XCTAssertTrue(row1.contains(column: "value"))
+        XCTAssertFalse(try row1.decodeNil(column: "value"))
+        XCTAssertEqual(try row1.decode(column: "value", as: String?.self), "abc")
+        XCTAssertThrowsError(try row1.decode(column: "value", as: Int.self))
+        XCTAssertThrowsError(try row1.decode(column: "nonexistent", as: String?.self))
+        XCTAssertThrowsError(try row1.decodeNil(column: "nonexistent"))
+        XCTAssertTrue(row2.contains(column: "value"))
+        XCTAssertTrue(try row2.decodeNil(column: "value"))
+        XCTAssertEqual(try row2.decode(column: "value", as: String?.self), nil)
+        XCTAssertThrowsError(try row2.decode(column: "value", as: Int.self))
+        XCTAssertThrowsError(try row2.decode(column: "nonexistent", as: String?.self))
+        XCTAssertThrowsError(try row2.decodeNil(column: "nonexistent"))
+    }
+    
+    func testRowEncoding() async throws {
+        struct SubFoo: Codable {
+            struct NestFoo: Codable { let x: Double }
+            let arr: [Int]
+            let val: String
+            let nest: NestFoo
+        }
+        try await self.db.create(table: "foo")
+            .column("id", type: .int, .primaryKey(autoIncrement: false), .notNull)
+            .column("value", type: .custom(SQLRaw("json")))
+            .run()
+        try await self.db.insert(into: "foo")
+            .columns("id", "value")
+            .values(SQLLiteral.numeric("1"), SQLBind(SubFoo(arr: [1,2,3], val: "a", nest: .init(x: 1.1))))
+            .values(SQLLiteral.numeric("2"), SQLBind(SubFoo?.none))
+            .run()
+        let rows = try await self.db.select()
+            .column(self.db.dialect.nestedSubpathExpression(in: SQLColumn("value"), for: ["nest", "x"])!, as: "x")
+            .from("foo")
+            .orderBy("id")
+            .all()
+        
+        XCTAssertEqual(rows.count, 2)
+        let row1 = try XCTUnwrap(rows.dropFirst(0).first),
+            row2 = try XCTUnwrap(rows.dropFirst(1).first)
+        XCTAssertTrue(row1.contains(column: "x"))
+        XCTAssertTrue(row2.contains(column: "x"))
+        XCTAssertEqual(try row1.decode(column: "x", as: Double.self), 1.1)
+        XCTAssertTrue(try row2.decodeNil(column: "x"))
+    }
+    
     // https://github.com/vapor/sqlite-kit/issues/62
     func testEncodeNestedArray() throws {
         struct Foo: Encodable {
@@ -159,23 +232,23 @@ final class SQLiteKitTests: XCTestCase {
     var threadPool: NIOThreadPool!
     var connection: SQLiteConnection!
 
-    override func setUp() {
+    override func setUp() async throws {
         XCTAssertTrue(isLoggingConfigured)
         self.eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 2)
         self.threadPool = NIOThreadPool(numberOfThreads: 2)
         self.threadPool.start()
-        self.connection = try! SQLiteConnectionSource(
+        self.connection = try await SQLiteConnectionSource(
             configuration: .init(storage: .memory, enableForeignKeys: true),
             threadPool: self.threadPool
-        ).makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).wait()
+        ).makeConnection(logger: .init(label: "test"), on: self.eventLoopGroup.any()).get()
     }
 
-    override func tearDown() {
-        try! self.connection.close().wait()
+    override func tearDown() async throws {
+        try await self.connection.close().get()
         self.connection = nil
-        try! self.threadPool.syncShutdownGracefully()
+        try await self.threadPool.shutdownGracefully()
         self.threadPool = nil
-        try! self.eventLoopGroup.syncShutdownGracefully()
+        try await self.eventLoopGroup.shutdownGracefully()
         self.eventLoopGroup = nil
     }
 }
