631b856af805c8ee52e6546771cb255d2db7057c
Gwynne Raskind <gwynne@vapor.codes>
2024-05-10 19:43:04 -0500

    Add back (deprecated) direct conformance to SQLRow to SQLiteRow (removing the conformance was source-breaking)

--- Sources/SQLiteKit/SQLiteRow+SQLRow.swift
+++ Sources/SQLiteKit/SQLiteRow+SQLRow.swift
@@ -51,3 +51,40 @@ private struct SQLiteSQLRow: SQLRow {
         return try self.decoder.decode(D.self, from: data)
     }
 }
+
+/// A legacy deprecated conformance of `SQLiteRow` directly to `SQLRow`. This interface exists solely
+/// because its absence would be a public API break.
+///
+/// Do not use these methods directly. Call `sql(decoder:)` instead to access `SQLiteRow`s through
+/// an `SQLKit` interface.
+@available(*, deprecated, message: "Use SQLiteRow.sql(decoder:) to access an SQLiteRow as an SQLRow.")
+extension SQLiteNIO.SQLiteRow: SQLKit.SQLRow {
+    // See `SQLRow.allColumns`.
+    public var allColumns: [String] { self.columns.map { $0.name } }
+
+    // See `SQLRow.contains(column:)`.
+    public func contains(column: String) -> Bool { self.column(column) != nil }
+
+    // See `SQLRow.decodeNil(column:)`.
+    public func decodeNil(column: String) throws -> Bool { (self.column(column) ?? .null) == .null }
+
+    // See `SQLRow.decode(column:as:)`.
+    public func decode<D: Decodable>(column: String, as: D.Type) throws -> D {
+        guard let data = self.column(column) else { throw MissingColumn(column: column) }
+        return try SQLiteDataDecoder().decode(D.self, from: data)
+    }
+
+    // See `SQLRow.decode(column:inferringAs:)`.
+    public func decode<D: Decodable>(column c: String, inferringAs: D.Type = D.self) throws -> D { try self.decode(column: c, as: D.self) }
+
+    // See `SQLRow.decode(model:prefix:keyDecodingStrategy:userInfo:)`.
+    public func decode<D: Decodable>(
+        model: D.Type, prefix: String? = nil, keyDecodingStrategy: SQLRowDecoder.KeyDecodingStrategy = .useDefaultKeys,
+        userInfo: [CodingUserInfoKey: any Sendable] = [:]
+    ) throws -> D {
+        try self.decode(model: D.self, with: .init(prefix: prefix, keyDecodingStrategy: keyDecodingStrategy, userInfo: userInfo))
+    }
+
+    // See `SQLRow.decode(model:with:)`.
+    public func decode<D: Decodable>(model: D.Type, with: SQLRowDecoder) throws -> D { try with.decode(D.self, from: self) }
+}
