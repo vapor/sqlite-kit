20b9755ba7507401ce37b6b8c082ba1663c92c08
Gwynne Raskind <gwynne@vapor.codes>
2024-05-27 12:27:29 -0500

    Don't log double `[]`s when logging queries.

--- Sources/SQLiteKit/SQLiteConnection+SQLKit.swift
+++ Sources/SQLiteKit/SQLiteConnection+SQLKit.swift
@@ -218,7 +218,7 @@ struct SQLiteDatabaseVersion: SQLDatabaseReportedVersion {
         let (sql, rawBinds) = self.serialize(query)
         
         if let queryLogLevel = self.queryLogLevel {
-            self.logger.log(level: queryLogLevel, "\(sql) [\(rawBinds)]")
+            self.logger.log(level: queryLogLevel, "\(sql) \(rawBinds)")
         }
 
         let binds: [SQLiteData]
@@ -244,7 +244,7 @@ struct SQLiteDatabaseVersion: SQLDatabaseReportedVersion {
         let (sql, rawBinds) = self.serialize(query)
         
         if let queryLogLevel = self.queryLogLevel {
-            self.logger.log(level: queryLogLevel, "\(sql) [\(rawBinds)]")
+            self.logger.log(level: queryLogLevel, "\(sql) \(rawBinds)")
         }
 
         try await self.database.query(
